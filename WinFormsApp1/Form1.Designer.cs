60fb09219b3363b1edd934245b1ff33658e8f6fe
rudojr <rudo1706@gmail.com>
2023-10-02 13:37:06 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnLoadImg = new Button();
+            btnCompress = new Button();
+            domainBlockSize = new DomainUpDown();
+            domainThreshHold = new DomainUpDown();
+            domainQrate = new DomainUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(273, 416);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(279, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(271, 416);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // btnLoadImg
+            // 
+            btnLoadImg.Location = new Point(0, 422);
+            btnLoadImg.Name = "btnLoadImg";
+            btnLoadImg.Size = new Size(75, 23);
+            btnLoadImg.TabIndex = 2;
+            btnLoadImg.Text = "Chọn hình";
+            btnLoadImg.UseVisualStyleBackColor = true;
+            btnLoadImg.Click += btnLoadImg_Click;
+            // 
+            // btnCompress
+            // 
+            btnCompress.Location = new Point(475, 422);
+            btnCompress.Name = "btnCompress";
+            btnCompress.Size = new Size(75, 23);
+            btnCompress.TabIndex = 3;
+            btnCompress.Text = "Nén";
+            btnCompress.UseVisualStyleBackColor = true;
+            btnCompress.Click += btnCompress_Click;
+            // 
+            // domainBlockSize
+            // 
+            domainBlockSize.Location = new Point(164, 422);
+            domainBlockSize.Name = "domainBlockSize";
+            domainBlockSize.Size = new Size(56, 23);
+            domainBlockSize.TabIndex = 4;
+            domainBlockSize.Text = "domainUpDown1";
+            // 
+            // domainThreshHold
+            // 
+            domainThreshHold.Location = new Point(300, 422);
+            domainThreshHold.Name = "domainThreshHold";
+            domainThreshHold.Size = new Size(56, 23);
+            domainThreshHold.TabIndex = 5;
+            domainThreshHold.Text = "domainUpDown2";
+            // 
+            // domainQrate
+            // 
+            domainQrate.Location = new Point(410, 422);
+            domainQrate.Name = "domainQrate";
+            domainQrate.Size = new Size(59, 23);
+            domainQrate.TabIndex = 6;
+            domainQrate.Text = "domainUpDown3";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(102, 426);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 7;
+            label1.Text = "BlockSize";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(226, 426);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 8;
+            label2.Text = "ThreshHold";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(362, 426);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Q.Rate";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(549, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(domainQrate);
+            Controls.Add(domainThreshHold);
+            Controls.Add(domainBlockSize);
+            Controls.Add(btnCompress);
+            Controls.Add(btnLoadImg);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button btnLoadImg;
+        private Button btnCompress;
+        private DomainUpDown domainBlockSize;
+        private DomainUpDown domainThreshHold;
+        private DomainUpDown domainQrate;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System.Drawing.Imaging;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                domainBlockSize.Items.Add(i.ToString());
+                domainThreshHold.Items.Add(i.ToString());
+                domainQrate.Items.Add(i.ToString());
+            }
+
+            domainBlockSize.SelectedItem = "4";
+            domainQrate.SelectedItem = "8";
+            domainThreshHold.SelectedItem = "6";
+        }
+
+        private void btnLoadImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Chọn hình ảnh";
+            openFile.Filter = "Tất cả các tệp|*.*|Hình ảnh|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string selectedImagePath = openFile.FileName;
+                    Image selectedImage = Image.FromFile(selectedImagePath);
+                    Image resizedImage = ResizeImage(selectedImage, pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = resizedImage;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi mở hình ảnh: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private Image ResizeImage(Image image, int width, int height)
+        {
+            Bitmap resizedImage = new Bitmap(width, height);
+            using (Graphics graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.DrawImage(image, 0, 0, width, height);
+            }
+            return resizedImage;
+        }
+
+        private ImageCodecInfo GetJpegCodecInfo()
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == ImageFormat.Jpeg.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private Image CompressBitmapToJpeg(Image image, int threshold, int blockSize, int qualityRate)
+        {
+            // Tạo đối tượng EncoderParameter để thiết lập các tham số nén
+            EncoderParameter qualityParam = new EncoderParameter(Encoder.Quality, qualityRate);
+            EncoderParameter thresholdParam = new EncoderParameter(Encoder.Compression, threshold);
+            EncoderParameter blockSizeParam = new EncoderParameter(Encoder.ColorDepth, blockSize);
+
+            // Tạo mảng EncoderParameters chứa các tham số nén
+            EncoderParameters encoderParams = new EncoderParameters(3);
+            encoderParams.Param[0] = qualityParam;
+            encoderParams.Param[1] = thresholdParam;
+            encoderParams.Param[2] = blockSizeParam;
+
+            String outputImagePath = @"D:\ALl\img_compress.jpeg";
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Tạo một Bitmap mới từ ảnh gốc
+                Bitmap inputBitmap = new Bitmap(image);
+                // Lưu Bitmap thành tệp JPEG với các tham số nén
+                inputBitmap.Save(ms, GetJpegCodecInfo(), encoderParams);
+                // Đọc lại ảnh từ MemoryStream và trả về nó
+                return Image.FromStream(ms);
+            }
+
+        }
+
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            Image inputImage = pictureBox1.Image;
+
+            if (inputImage != null)
+            {
+                int threshold = int.Parse(domainThreshHold.SelectedItem.ToString());
+                int blocksize = int.Parse(domainBlockSize.SelectedItem.ToString());
+                int qrate = int.Parse(domainQrate.SelectedItem.ToString());
+
+                Image compressImg = CompressBitmapToJpeg(inputImage, threshold, qrate, blocksize);
+
+                Image resizeCompress = ResizeImage(compressImg, pictureBox2.Width, pictureBox2.Height);
+                pictureBox2.Image = resizeCompress;
+                SaveImageFromPictureBox2();
+            }
+        }
+
+        private void SaveImageFromPictureBox2()
+        {
+            Image imageToSave = pictureBox2.Image;
+
+            if (imageToSave != null)
+            {
+                string outputPath = @"D:\image_output.jpg"; // Đường dẫn và tên tệp đích
+                imageToSave.Save(outputPath, ImageFormat.Jpeg);
+
+                MessageBox.Show("Ảnh đã được lưu tại:\n" + outputPath, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Không có ảnh để lưu.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
